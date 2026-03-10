37d0f50a885d451966d1abc36bfb9e35f4e0fd06
Нефёдов Александр <alexnef1234@gmail.com>
2026-03-10 12:12:37 +0300

    2

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -3,11 +3,15 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=256, null=False, blank=False, verbose_name='Заголовок')
-    description = models.TextField(null=False, blank=False, verbose_name='Описание')
-    slug = models.SlugField(null=False, blank=False, unique=True, verbose_name='Идентификатор',
-                            help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
-    is_published = models.BooleanField(default=True, null=False, blank=False, verbose_name='Опубликовано',
+    title = models.CharField(max_length=256, null=False, blank=False,
+                             verbose_name='Заголовок')
+    description = models.TextField(null=False, blank=False,
+                                   verbose_name='Описание')
+    slug = models.SlugField(null=False, blank=False, unique=True,
+                            verbose_name='Идентификатор',
+                            help_text='Идентификатор страницы для URL; '
+                                      'разрешены символы латиницы, цифры, дефис и подчёркивание.')
+    is_published = models.BooleanField(default=True, null=False,blank=False, verbose_name='Опубликовано',
                                        help_text='Снимите галочку, чтобы скрыть публикацию.')
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False, verbose_name='Добавлено')
 
@@ -37,7 +41,8 @@ class Post(models.Model):
     title = models.CharField(max_length=256, null=False, blank=False, verbose_name='Заголовок')
     text = models.TextField(null=False, blank=False, verbose_name='Текст')
     pub_date = models.DateTimeField(null=False, blank=False, verbose_name='Дата и время публикации',
-                                    help_text='Если установить дату и время в будущем — можно делать отложенные публикации.')
+                                    help_text='Если установить дату и время в будущем '
+                                              '— можно делать отложенные публикации.')
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, verbose_name='Автор публикации')
     location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL, blank=True,
                                  verbose_name='Местоположение')
