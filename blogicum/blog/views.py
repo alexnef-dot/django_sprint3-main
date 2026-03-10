90a23e44b2170ae2397ee12caadf8898bb3159ad
Нефёдов Александр <alexnef1234@gmail.com>
2026-03-10 12:41:10 +0300

    11

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -6,19 +6,20 @@ from django.utils import timezone
 
 def index(request):
     template = "blog/index.html"
-    posts = Post.objects.filter(is_published = True,
-                                category__is_published = True,
-                                pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
-    context = {"post_list":posts}
+    posts = Post.objects.filter(is_published=True,
+    category__is_published=True,
+    pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    context = {"post_list": posts}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
-    category = get_object_or_404(Category, slug = category_slug)
+    category = get_object_or_404(Category, slug=category_slug)
     if not category.is_published:
         raise Http404("Эта категория не опубликована")
     template = "blog/category.html"
-    posts = Post.objects.filter(is_published = True, pub_date__lte = timezone.now(), category = category)
+    posts = Post.objects.filter(is_published=True,
+    pub_date__lte=timezone.now(), category=category)
     context = {
         "category": category,  # Добавлено для использования в шаблоне
         "post_list": posts  # или "posts": posts
@@ -27,13 +28,13 @@ def category_posts(request, category_slug):
 
 
 def post_detail(request, id):
-    post = get_object_or_404(Post, id = id)
+    post = get_object_or_404(Post, id=id)
     if not post.is_published:
         raise Http404("Эта пост не опубликован")
     if  not post.category.is_published:
         raise Http404("Эта категория не опубликована")
     if post.pub_date > timezone.now():
         raise Http404("Эта пост не опубликован по времени")
-    context = {"post":post}
+    context = {"post": post}
     template = "blog/detail.html"
     return render(request, template, context)
